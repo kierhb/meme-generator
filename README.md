b7b90fe57d892a5c868dd83e36776aa202e27676
kierhb <kierbobila@gmail.com>
2022-12-30 10:18:13 +0800

    Build a meme generator app

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# meme-generator
+# Meme Generator
 In section 3, Bob guides you through building his classic meme generator, updated with the newest React features.
+
+## Table of contents
+- [Introduction](#introduction)
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Built using](#built-using)
+  - [Lessons Learned](#lessons-learned)
+- [Author](#author)
+- [Acknowledgement](#acknowledgement)
+- [Additional Note](#additional-note)
+
+## Introduction
+- This is my solution to the second part of [Learn React](https://scrimba.com/learn/learnreact).
+- This webpage was setup using [Vite](https://vitejs.dev/).
+- This webpage features:
+    - Interactive components
+    - API calls
+
+## Overview
+- Link: [github.com/kierhb/meme-generator](https://github.com/kierhb/meme-generator)
+
+### Screenshot
+![screenshot](./meme-generator.jpg)
+![active 1](./meme-generator-active-1.jpg)
+
+### Built using
+
+- HTML5 markup
+- CSS
+- Vanilla JavaScript
+- ReactJS
+
+### Lessons Learned
+
+- Adding Event listeners
+- State
+- Conditional Rendering
+- Forms:
+    - Text area
+    - Checkbox
+    - Radio buttons
+    - Select & Option
+    - Submitting the form
+- Understanding Side effects
+- Making API calls
+
+## Author
+
+- GitHub - [@kierhb](https://github.com/kierhb)
+- LinkedIn - [Kier Bobila](https://www.linkedin.com/in/kier-bobila/)
+
+## Acknowledgement
+I want to acknowledge the course instructor Bob Ziroll and to all members of Scrimba community.
+
+## Additional Note
+Not all memes are applicable for this app, only those with upper and lower text boxes only.
+
+![active 2](./meme-generator-active-2.jpg)
---
+++ meme-generator/src/components/Meme.jsx
@@ -0,0 +1,75 @@
+import React from "react"
+
+
+function Meme() {
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemes, setAllMemes] = React.useState([])
+
+    React.useState(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
+
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+        setMeme(prevMeme => {
+            return ({
+                ...prevMeme,
+                randomImage: url
+            })
+        })
+    }
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                [name]: value
+            }
+        })
+    }
+
+    return (
+        <main>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                    name="topText"
+                    vale={meme.topText}
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                    name="bottomText"
+                    vale={meme.bottomText}
+                    onChange={handleChange}
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
+        </main>
+    )
+}
+
+export default Meme
